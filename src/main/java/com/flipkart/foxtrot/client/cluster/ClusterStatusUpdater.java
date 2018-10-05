8a4d5e9eb535b401928babe6fedb9144b4dec514
Nitish Goyal <nitish.goyal@phonepe.com>
2018-10-05 07:54:05 +0530

    Fixed warnings

--- src/main/java/com/flipkart/foxtrot/client/cluster/ClusterStatusUpdater.java
+++ src/main/java/com/flipkart/foxtrot/client/cluster/ClusterStatusUpdater.java
@@ -19,9 +19,9 @@ public class ClusterStatusUpdater implements Runnable {
 
     private AtomicReference<FoxtrotClusterStatus> status;
 
-    final static JacksonDecoder decoder = new JacksonDecoder();
-    final static JacksonEncoder encoder = new JacksonEncoder();
-    final static Slf4jLogger slf4jLogger = new Slf4jLogger();
+    private final static JacksonDecoder decoder = new JacksonDecoder();
+    private final static JacksonEncoder encoder = new JacksonEncoder();
+    private final static Slf4jLogger slf4jLogger = new Slf4jLogger();
 
     private ClusterStatusUpdater(AtomicReference<FoxtrotClusterStatus> status, FoxtrotClusterHttpClient httpClient) {
         this.status = status;
@@ -29,7 +29,7 @@ public class ClusterStatusUpdater implements Runnable {
     }
 
     public static ClusterStatusUpdater create(FoxtrotClientConfig config,
-                                              AtomicReference<FoxtrotClusterStatus> status) throws Exception {
+                                              AtomicReference<FoxtrotClusterStatus> status) {
 
         FoxtrotClusterHttpClient httpClient = Feign.builder()
                 .decoder(decoder)
