c48b2fad2b165f4cc91fd5dd2d55d3ae7b46bc4f
Nitish Goyal <nitish.goyal@phonepe.com>
2018-10-05 07:43:19 +0530

    Removed redunant public keywords

--- src/main/java/com/flipkart/foxtrot/client/serialization/EventSerializationHandler.java
+++ src/main/java/com/flipkart/foxtrot/client/serialization/EventSerializationHandler.java
@@ -5,9 +5,9 @@ import com.flipkart.foxtrot.client.Document;
 import java.util.List;
 
 public interface EventSerializationHandler {
-    public byte[] serialize(Document document) throws SerializationException;
+    byte[] serialize(Document document) throws SerializationException;
 
-    public byte[] serialize(List<Document> documents) throws SerializationException;
+    byte[] serialize(List<Document> documents) throws SerializationException;
 
-    public Document deserialize(byte[] data) throws DeserializationException;
+    Document deserialize(byte[] data) throws DeserializationException;
 }
